62fc61d8a0f21386449183e24619996ffcd351dc
Matheus Konorat <konorat.matheus@gmail.com>
2023-04-21 09:16:15 -0300

    Cliente, TipoCliente, Telefones e Enderecos

---
+++ src/main/java/com/konorat/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.konorat.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.konorat.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+}
