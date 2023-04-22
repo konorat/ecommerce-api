1434b10b1c7122efdd06e1a14aeabc155c21446f
Matheus Konorat <konorat.matheus@gmail.com>
2023-04-22 17:25:40 -0300

    Endpoint /pedidos/{id] disponibilizado

--- src/main/java/com/konorat/cursomc/domain/Pagamento.java
+++ src/main/java/com/konorat/cursomc/domain/Pagamento.java
@@ -3,6 +3,7 @@ package com.konorat.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.konorat.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -22,6 +23,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="PEDIDO_ID")
 	@MapsId
---
+++ src/main/java/com/konorat/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.konorat.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.konorat.cursomc.domain.Pedido;
+import com.konorat.cursomc.repositories.PedidoRepository;
+import com.konorat.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", tipo: " + Pedido.class.getName()));
+		}
+	
+}
