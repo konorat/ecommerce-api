55a7b88c5cd05680af0af5446a8a33a2b5efe645
Matheus Konorat <konorat.matheus@gmail.com>
2023-04-25 11:02:13 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/konorat/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,41 @@
+package com.konorat.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.konorat.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/konorat/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/konorat/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.konorat.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.konorat.cursomc.domain.Categoria;
+import com.konorat.cursomc.dto.CategoriaDTO;
 import com.konorat.cursomc.services.CategoriaService;
 
 @RestController
@@ -28,6 +31,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 			Categoria obj = service.find(id);		
 			return ResponseEntity.ok().body(obj);			
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+			List<Categoria> list = service.findAll();		
+			List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);			
 	}
 	
 	@PostMapping
