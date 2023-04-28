29de46a5fa6ea326cc2f1e3f51a5b9d6acb55190
Matheus Konorat <konorat.matheus@gmail.com>
2023-04-28 16:09:19 -0300

    Inserindo Pedido

--- src/main/java/com/konorat/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/konorat/cursomc/domain/PagamentoComCartao.java
@@ -1,30 +1,32 @@
 package com.konorat.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.konorat.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
-	private Integer numeroDePacelas;
+	private Integer numeroDeParcelas;
 	
 	public PagamentoComCartao() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDePacelas) {
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
 		super(id, estado, pedido);
-		this.numeroDePacelas = numeroDePacelas;
+		this.numeroDeParcelas = numeroDeParcelas;
 	}
 
-	public Integer getNumeroDePacelas() {
-		return numeroDePacelas;
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
 	}
 
-	public void setNumeroDePacelas(Integer numeroDePacelas) {
-		this.numeroDePacelas = numeroDePacelas;
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
 	}
 	
 	
--- src/main/java/com/konorat/cursomc/resources/PedidoResource.java
+++ src/main/java/com/konorat/cursomc/resources/PedidoResource.java
@@ -1,14 +1,21 @@
 package com.konorat.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.konorat.cursomc.domain.Pedido;
 import com.konorat.cursomc.services.PedidoService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value="/pedidos")
@@ -22,6 +29,13 @@ public class PedidoResource {
 
 			Pedido obj = service.find(id);		
 			return ResponseEntity.ok().body(obj);			
-		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
