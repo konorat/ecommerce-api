c30619016f0e1fc634c8e4d4e1b2ab14fe8b4c1b
Matheus Konorat <konorat.matheus@gmail.com>
2023-05-04 13:20:58 -0300

    Email HTML com Thymeleaf

--- src/main/java/com/konorat/cursomc/services/EmailService.java
+++ src/main/java/com/konorat/cursomc/services/EmailService.java
@@ -4,9 +4,15 @@ import org.springframework.mail.SimpleMailMessage;
 
 import com.konorat.cursomc.domain.Pedido;
 
+import jakarta.mail.internet.MimeMessage;
+
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/konorat/cursomc/services/MockEmailService.java
+++ src/main/java/com/konorat/cursomc/services/MockEmailService.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
+import jakarta.mail.internet.MimeMessage;
+
 public class MockEmailService extends AbstractEmailService {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
@@ -14,5 +16,13 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info("Simulando envio de email");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
 	}
 }
