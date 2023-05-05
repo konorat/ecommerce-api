70950505d87396ce216252fa9c63a37b568525fa
Matheus Konorat <konorat.matheus@gmail.com>
2023-05-05 15:34:27 -0300

    Adicionando senha encriptada com BCrypt ao Cliente

--- src/main/java/com/konorat/cursomc/config/SecurityConfig.java
+++ src/main/java/com/konorat/cursomc/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -59,6 +60,13 @@ public class SecurityConfig {
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+		
+	}
+	
 }
 
 
