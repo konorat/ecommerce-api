a963d3f91eea130be2f5b784a8fff12b97cb3283
Matheus Konorat <konorat.matheus@gmail.com>
2023-07-05 13:55:58 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/konorat/cursomc/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.konorat.cursomc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	};
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.cors().and().csrf().disable();
+	    http
+	      .authorizeHttpRequests()
+	      .requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+	      .requestMatchers(PUBLIC_MATCHERS).permitAll()
+	      .anyRequest().authenticated()
+	      .and()
+	      .httpBasic()
+	      .and()
+	      .sessionManagement()
+	      .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	    return http.build();
+	}
+	
+	//permite requisiçoes multiplas
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+	
+	
+	//libera h2-console
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+	    return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+	}
+}
